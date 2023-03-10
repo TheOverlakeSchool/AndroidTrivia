47c3bd8204b18a82cb2df07116531985a09ea04f
zerbash <dbasham@overlake.org>
2023-03-10 14:42:34 -0800

    3-10-23

--- app/src/main/java/com/example/android/navigation/MainActivity.java
+++ app/src/main/java/com/example/android/navigation/MainActivity.java
@@ -16,7 +16,9 @@
 
 package com.example.android.navigation;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,13 +27,31 @@ import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RateUsFragment.Listener {
+
+    public static final String RATED = "rated";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         DataBindingUtil.setContentView(this, R.layout.activity_main);
         FragmentManager fm = getSupportFragmentManager();
         NavController controller = ((NavHostFragment) fm.findFragmentById(R.id.fragmentContainerView)).getNavController();
-        controller.navigate(R.id.rateUsFragment);
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        if (!preferences.getBoolean(RATED, false)) {
+            controller.navigate(R.id.rateUsFragment);
+            preferences.edit().putBoolean(RATED, true).apply();
+        }
+    }
+
+    @Override
+    public void onClick(float rating) {
+        String msg;
+        if (rating > 4) {
+            msg = "This is going to be easy!";
+        } else {
+            msg = "Heh, good luck with this";
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/android/navigation/RateUsFragment.java
+++ app/src/main/java/com/example/android/navigation/RateUsFragment.java
@@ -2,6 +2,7 @@ package com.example.android.navigation;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
@@ -9,20 +10,39 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-public class RateUsFragment extends DialogFragment {
+import com.example.android.navigation.databinding.RateUsBinding;
+
+public class RateUsFragment extends DialogFragment implements DialogInterface.OnClickListener {
+
+    private com.example.android.navigation.databinding.RateUsBinding mBinding;
+
+    interface Listener {
+        void onClick(float rating);
+    }
+
+    Listener mListener;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        mBinding = RateUsBinding.inflate(getLayoutInflater());
         return new AlertDialog.Builder(getContext())
-                .setMessage("Hello!")
-              //  .setView(R.layout.rate_us)
-                .setPositiveButton("Yes!", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int i) {
-
-                    }
-                })
-                .setNegativeButton("Cancel", null)
+                .setMessage(R.string.rate_us)
+                .setView(mBinding.getRoot())
+                .setPositiveButton("Submit", this)
+                .setNegativeButton("Cancel", this)
                 .create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int i) {
+        float rating = mBinding.ratingBar.getRating();
+        mListener.onClick(rating);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mListener = (Listener) context;
     }
 }
